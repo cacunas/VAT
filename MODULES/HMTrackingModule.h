241c16516b6e3ffb1bd54505e2197a9747e8f2d9
laloqwe <d.cardenas.nl@hmail.com>
2014-07-25 13:53:40 -0400

    Update HMTrackingModule.h
    Agruegue 2 metodos:
    
        //Get the line on the grass selection usign Hough Transform
        void Line_detect();
    
        //Aplly a filter
        void ApplyFilter(QImage *f_in, QImage *f_out);

--- MODULES/HMTrackingModule.h
+++ MODULES/HMTrackingModule.h
@@ -11,7 +11,15 @@
 #include "Datapool.h"
 #include "ModuleInterface.h"
 
+#include "opencv2/opencv.hpp"
+#include <opencv/highgui.h>
+#include <map>
+#include <string>
+#include "image_display.h"
+
 typedef std::vector<int> hist;
+
+using namespace std;
 
 class HMTrackingModule : public ModuleInterface
 {
@@ -34,20 +42,36 @@ public:
 private:
     bool firstTime;
     float alpha, beta;
-    float R_p, G_p, B_p, GL_p; // histogram intensity peaks
-    float R_t, G_t, B_t, GL_t; // intensity thresholds
-    float R_u, G_u, B_u, GL_u; // histogram intensity mean
-    float R_std, G_std, B_std, GL_std; //histogram standar deviation
+    float A_p[4];//, G_p, B_p, GL_p; // histogram intensity peaks
+    float A_t[4];//, G_t, B_t, GL_t; // intensity thresholds
+    //float R_u, G_u, B_u, GL_u; // histogram intensity mean
+    //float R_std, G_std, B_std, GL_std; //histogram standar deviation
 
 
     //Convert QImage to cv::Mat
     cv::Mat qimage_to_mat_cpy(QImage* img, int format);
 
+    //Calculates Intensity Histograms per channel
+    vector<hist> calculateHistograms(QImage* img);
+
+    //Calculates first and second statistical moments of the intesity histograms
+    vector<float> calculateMoments(vector<hist> channels);
+
+    //Calculate channels peaks using paper proposed criterias
+    void calculatePeaks(vector<hist> channels);
+
+    //Calculate thresholds
+    void calculateThresholds(vector<hist> ch);
+
     //Store grass area of the image on bgImage using a binary classifier
     void GrassClassifier();
 
-    //Calculate channels peaks using paper proposed criterias
-    void calculatePeaks(hist r, hist g, hist b, hist gray);
+    //Get the line on the grass selection usign Hough Transform
+    void Line_detect();
+
+    //Aplly a filter
+    void ApplyFilter(QImage *f_in, QImage *f_out);
+
 };
 
 #endif // HMTRACKINGMODULE_H
